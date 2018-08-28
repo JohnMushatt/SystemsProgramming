445beaa0f547d510556b38ed8324eb6e2041ecf1
JohnMushatt <john.mushatt@gmail.com>
2018-08-28 19:49:21 -0400

    updated lab1

---
+++ cs2303/lab1/triangle.c
@@ -0,0 +1,10 @@
+/*triangl.c -- John Mushatt */
+#include <stdio.h>
+
+int main() {
+  double xA, yA, xB, yB, xC, yC;
+  printf("Enter the x- and y-coordinates of point A: ");
+  scanf("%f %f\n",xA,yA );
+  printf("%s\tasdasdasd\t%s\n",xA,yA );
+
+}
