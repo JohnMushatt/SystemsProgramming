3f8043df8d2eb1ef697105456588918bbb979177
JohnMushatt <john.mushatt@gmail.com>
2018-08-28 21:49:11 -0400

    Finished part 1 and part 2

--- cs2303/lab1/triangle.c
+++ cs2303/lab1/triangle.c
@@ -1,10 +1,28 @@
 /*triangl.c -- John Mushatt */
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-  double xA, yA, xB, yB, xC, yC;
-  printf("Enter the x- and y-coordinates of point A: ");
-  scanf("%f %f\n",xA,yA );
-  printf("%s\tasdasdasd\t%s\n",xA,yA );
+  float xA, yA, xB, yB, xC, yC;
+  float lengthAB, lengthBC, lengthCA;
+  float circumference, area, perimeter;
 
+  printf("Enter the x- and y-coordinates of point A: ");
+  scanf("%f %f",&xA,&yA );
+  printf("Enter the x- and y-coordinates of point B: ");
+  scanf("%f %f",&xB,&yB );
+  printf("Enter the x- and y-coordinates of point C: ");
+  scanf("%f %f",&xC,&yC );
+
+  lengthAB = sqrt(pow((xA-xB),2) + pow((yA-yB),2));
+  lengthBC = sqrt(pow((xB-xC),2) + pow((yB-yC),2));
+  lengthCA = sqrt(pow((xC-xA),2) + pow((yC-yA),2));
+
+  circumference = (lengthAB + lengthBC + lengthCA);
+  perimeter = .5 * (lengthAB + lengthBC + lengthCA);
+  area = sqrt(perimeter * (perimeter-lengthAB) * (perimeter-lengthBC) *
+  (perimeter-lengthCA));
+  printf("Length of AB is %.3f\nLength of BC is %.3f\nLength of CA is %.3f"
+  "\nCircumference is %.3f\nArea is %.3f",lengthAB,lengthBC,lengthCA,circumference,area);
+  return 0;
 }
